eadaf9486bf9133ee290c9c6077717989cbb4da0
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-07-03 00:14:27 -0700

    Basic Console IO
    Basic Console IO Added!

--- SimpleCSharpConsoleApp/SimpleCSharpConsoleApp/Program.cs
+++ SimpleCSharpConsoleApp/SimpleCSharpConsoleApp/Program.cs
@@ -8,6 +8,18 @@ namespace SimpleCSharpConsoleApp
 {
     class Program
     {
+        static void ShowEnvironmentDetails() {
+            // Print out the drives on this machine,
+            // and other interesting details.
+            foreach(string drive in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine("Drive: {0}", drive);
+            }
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("Number of processors: {0}", Environment.ProcessorCount);
+            Console.WriteLine(".NET Version: {0}", Environment.Version);
+        }
+
         static int Main(string[] args)
         {
             Console.Title = "My First App";
@@ -17,6 +29,24 @@ namespace SimpleCSharpConsoleApp
             Console.WriteLine("*** Welcome to My First App ***");
             Console.WriteLine("*******************************");
             Console.BackgroundColor = ConsoleColor.Black;
+
+            // Processing Command line arguments
+            for(int i=0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg: {0}", args[i]);
+            }
+
+            // Using foreach loop
+            foreach (string arg in args)
+                Console.WriteLine("Arg : {0}", arg);
+
+            // Get arguments using System.Environment
+            string[] theArgs = Environment.GetCommandLineArgs();
+            foreach(string arg in theArgs)
+                Console.WriteLine("Arg: {0}", arg);
+
+            // Helper method within the Program class.
+            ShowEnvironmentDetails();
 
             // Wait for the key to be pressed
             Console.ReadLine();
