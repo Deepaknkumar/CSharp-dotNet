11f3807163d0219229f7e3bc60955597653e1e07
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-07-02 22:25:15 -0700

    First Console App
    First Console App Added!

---
+++ SimpleCSharpConsoleApp/SimpleCSharpConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpConsoleApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Title = "My First App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*******************************");
+            Console.WriteLine("*** Welcome to My First App ***");
+            Console.WriteLine("*******************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            // Wait for the key to be pressed
+            Console.ReadLine();
+
+            // Return an arbitrary error code
+            return -1;
+        }
+    }
+}
